83cfaa978f87a50d2d350e4b422915cad05726e2
Alex Loosley <a.loosley@reply.de>
2017-09-06 19:03:29 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='persistable',
-    version='0.1.11',
+    version='0.1.12',
     packages=['persistable', 'persistable/util'],
     url='https://github.com/DataReply/persistable',
-    download_url='https://github.com/DataReply/persistable/archive/0.1.11.tar.gz',
+    download_url='https://github.com/DataReply/persistable/archive/0.1.12.tar.gz',
     license='',
     author='Alex Loosley',
     author_email='a.loosley@reply.de',
