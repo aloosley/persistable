e60401aebeb4ab1d26863f7a7d6585be37cecba6
Alex Loosley <a.loosley@reply.de>
2017-09-06 10:31:37 +0200

    added download URL to setup

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ setup(
     version='0.1.1',
     packages=['persistable'],
     url='https://github.com/DataReply/persistable',
+    download_url='https://github.com/DataReply/persistable/archive/0.1.1.tar.gz',
     license='',
     author='Alex Loosley',
     author_email='a.loosley@reply.de',
     description='An inheritable superclass with logging, and tools for persisting and '
                 'loading models with parameter tracking',
-    keywords = ['persisting', 'models', 'pipeline']
+    keywords = ['persisting', 'models', 'pipeline'],
     install_requires=[
         "cytoolz>=0.8.2",
         "wrapt>=1.10.11"
