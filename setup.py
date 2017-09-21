2b15dccd2546a01c89abdeee74f0d5be67711b4e
Alex Loosley <a.loosley@reply.de>
2017-09-21 18:13:47 +0200

    new release, added Verbose flag

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='persistable',
-    version='0.3.0',
+    version='0.3.1',
     packages=['persistable', 'persistable/util'],
     url='https://github.com/DataReply/persistable',
-    download_url='https://github.com/DataReply/persistable/archive/0.3.0.tar.gz',
+    download_url='https://github.com/DataReply/persistable/archive/0.3.1.tar.gz',
     license='',
     author='Alex Loosley',
     author_email='a.loosley@reply.de',
