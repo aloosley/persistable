cd338e3f0d9e55aaf98fef36247cfdc7de9b1102
Alex Loosley <aloosley@alumni.brown.edu>
2018-11-01 14:52:00 +0100

    updated version, new version has tests

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='persistable',
-    version='0.5.6',
+    version='0.5.7',
     packages=['persistable', 'persistable/util'],
     url='https://github.com/DataReply/persistable',
-    download_url='https://github.com/DataReply/persistable/archive/0.5.6.tar.gz',
+    download_url='https://github.com/DataReply/persistable/archive/0.5.7.tar.gz',
     license='',
     author='Alex Loosley, Stephan Sahm',
     author_email='a.loosley@reply.de, s.sahm@reply.de',
