761f1cf84df1f7c6a1358001accfee6ec8f6cddd
Alex Loosley <aloosley@alumni.brown.edu>
2018-12-27 10:46:03 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='persistable',
-    version='0.5.7',
+    version='0.5.8',
     packages=['persistable', 'persistable/util'],
     url='https://github.com/DataReply/persistable',
-    download_url='https://github.com/DataReply/persistable/archive/0.5.7.tar.gz',
+    download_url='https://github.com/DataReply/persistable/archive/0.5.8.tar.gz',
     license='',
     author='Alex Loosley, Stephan Sahm',
     author_email='a.loosley@reply.de, s.sahm@reply.de',
