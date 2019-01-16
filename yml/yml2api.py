26a7a063b3dbf40e9c78b0031542e013eede3bc7
Alex Loosley <a.loosley@reply.de>
2019-01-16 19:28:12 +0100

    yml2api now builds persistables from dependencies

--- yml/yml2api.py
+++ yml/yml2api.py
@@ -8,7 +8,49 @@ def load_yaml(yamlpath: Path) -> dict:
 
     return parsed_yml
 
-def get_code_from_yml(object_name: str, object: dict, super_class="Persistable") -> str:
+
+def _parse_inside_List(dep):
+    """
+
+    Parameters
+    ----------
+    dep
+
+    Returns
+    -------
+    Class, extension, type
+    """
+    if dep.lower().find("list[") != -1:
+        # There is a list:
+        inside_List = dep.split("List")[-1].strip("[]")
+        return inside_List, "_list", "list"
+    else:
+        return dep, "", dep
+
+
+def get_code_from_yml(object_name: str, object: dict, super_object: dict, super_class="Persistable") -> str:
+
+    # Prepping dependency information to the code:
+    deps = [dep for dep in object["dependencies"]] if "dependencies" in object else []
+    parsed_deps = [_parse_inside_List(dep) for dep in deps]
+
+    init_dep_args = ", ".join(
+        [f"{super_object[parsd_dep[0]]['name']}{parsd_dep[1]}: {parsd_dep[2]}" for parsd_dep in parsed_deps]
+    ) if len(parsed_deps) else "intermediate_datapath: Path"
+
+    init_dep_doc_string = "".join(
+        [
+            f'''
+        {super_object[parsed_dep[0]]['name']}{parsed_dep[1]} : {parsed_dep[2]}
+            {parsed_dep[0]} dependency''' for parsed_dep in parsed_deps
+        ]
+    ) if len(parsed_deps) else '''
+        intermediate_datapath : Path
+            The path where intermediate data is persisted / loaded'''
+
+    super_init_dep_args = f"from_persistable=[{', '.join([super_object[parsed_dep[0]]['name']+parsed_dep[1] for parsed_dep in parsed_deps])}]" \
+        if len(parsed_deps) else "workingdatapath=intermediate_datapath"
+
     return (
         f'''
 class {object_name}({super_class}):
@@ -44,11 +86,11 @@ class {object_name}({super_class}):
                 f'''
     {dep}'''
                 for dep in object["dependencies"]
-            ] if "dependencies" in object else ["None"]
+            ] if "dependencies" in object else []
         ) +
         f'''
                
-    def __init__(params: dict, intermediate_datapath: Path, verbose: bool=True):
+    def __init__(params: dict, {init_dep_args}, verbose: bool=True):
         """
         Initiate persistable object.
         
@@ -68,7 +110,7 @@ class {object_name}({super_class}):
                 OPTIONS: {repr(object['required_parameters'][req_param_name]['options'])}'''
                 if 'options' in object['required_parameters'][req_param_name] else '')
                 for req_param_name in object["required_parameters"]
-            ] if "required_parameters" in object else ["None"]
+            ] if "required_parameters" in object else []
         ) +
         f'''
                 
@@ -82,14 +124,12 @@ class {object_name}({super_class}):
                 DEFAULT: {repr(object['optional_parameters'][opt_param_name].get('default', None))}
                 EXAMPLES: {repr(object['optional_parameters'][opt_param_name].get('examples', None))}'''
                 for opt_param_name in object["optional_parameters"]
-            ] if "optional_parameters" in object else ["None"]
+            ] if "optional_parameters" in object else []
         ) +
-        f'''
-               
-        intermediate_datapath : Path
-           The path where intermediate data is persisted / loaded
+        f'''     
+        {init_dep_doc_string}
         verbose : bool
-           Verbosity flag - False dumps WARNING level logs, True dumps info level logs.
+            Verbosity flag - False dumps WARNING level logs, True dumps info level logs.
         """
                 
         super().__init__(
@@ -106,7 +146,7 @@ class {object_name}({super_class}):
                 ),
                 params
             ),
-            workingdatapath=intermediate_datapath,
+            {super_init_dep_args},
             required_params={tuple(req_param for req_param in object.get("required_parameters", tuple()))},
             excluded_fn_params=[],
             verbose=verbose
@@ -140,16 +180,20 @@ class {object_name}({super_class}):
     )
 
 
-def recyml2api(parsed_yml: dict, subclass_indicator: str="sub_classes", super_class="Persistable"):
+def recyml2api(parsed_yml: dict, original_parsed_yml: dict, subclass_indicator: str="sub_classes", super_class="Persistable"):
 
     base = list(parsed_yml.keys())
 
     for key in base:
         if subclass_indicator in parsed_yml[key]:
-            yield from recyml2api(parsed_yml[key][subclass_indicator], super_class=key)
+            yield from recyml2api(parsed_yml[key][subclass_indicator], original_parsed_yml, super_class=key)
 
     yield from (
-        get_code_from_yml(key, parsed_yml[key], super_class=super_class) for key in base
+        get_code_from_yml(
+            object_name=key, object=parsed_yml[key],
+            super_object=original_parsed_yml,
+            super_class=super_class
+        ) for key in base
     )
 
 
@@ -171,4 +215,4 @@ def yml2api(yamlpath: Path, subclass_indicator: str="sub_classes"):
 
     parsed_yml = load_yaml(yamlpath)
 
-    yield from recyml2api(parsed_yml, subclass_indicator)
+    yield from recyml2api(parsed_yml, parsed_yml, subclass_indicator)
