df6ff4147546710f65b0911e8286d50a63853cd6
Alex Loosley <a.loosley@rubinstein-schmiedel.com>
2022-07-07 11:02:56 +0200

    pre-commit + flake config

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 from distutils.core import setup
 from typing import List
 
-install_requires: List[str] = [
-    "numpy>=1.23.0"
-]
+install_requires: List[str] = ["numpy>=1.23.0"]
 dev_requires: List[str] = [
     "pre-commit>=2.19.0",
     "pytest>=7.1.2",
 ]
 
 setup(
-    name='persistable',
-    version='1.0.0',
-    packages=['persistable', 'persistable/util'],
-    url='https://github.com/aloosley/persistable',
-    download_url='https://github.com/aloosley/persistable/archive/1.0.0.tar.gz',
-    license='',
-    author='Alex Loosley, Stephan Sahm',
-    author_email='aloosley@alumni.brown.edu',
-    description='An inheritable superclass with logging, and tools for persisting and '
-                'loading models with parameter tracking',
-    keywords = ['persisting', 'models', 'pipeline'],
+    name="persistable",
+    version="1.0.0",
+    packages=["persistable", "persistable/util"],
+    url="https://github.com/aloosley/persistable",
+    download_url="https://github.com/aloosley/persistable/archive/1.0.0.tar.gz",
+    license="",
+    author="Alex Loosley, Stephan Sahm",
+    author_email="aloosley@alumni.brown.edu",
+    description="An inheritable superclass with logging, and tools for persisting and "
+    "loading models with parameter tracking",
+    keywords=["persisting", "models", "pipeline"],
     install_requires=install_requires,
     extras_require=dict(dev=dev_requires),
     python_requires=">=3.8",
